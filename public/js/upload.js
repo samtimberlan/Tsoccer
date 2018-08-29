df48a1c0eb69b58251697a104a1b8199b9db4d73
DESKTOP-G6T2AL6\timud <timudoma@gmail.com>
2018-08-29 21:44:13 +0100

    Add upload js file

---
+++ public/js/upload.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+  $(".upload-btn").on("click", () => {
+    $("#uploadInput").click();
+  });
+
+  $("uploadInput").on("change", () => {
+    var uploadInput = $("uploadInput");
+  });
+
+  if (uploadInput.val() != "") {
+    var formData = new FormData();
+    formData.append("upload", uploadInput[0].files[0]);
+
+    $.ajax({
+      url: "/uploadFile",
+      type: "POST",
+      data: formData,
+      processData: false,
+      contentData: false,
+      success: function() {
+        uploadInput.val("");
+      }
+    });
+  }
+});
